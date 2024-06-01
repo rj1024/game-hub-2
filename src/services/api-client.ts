ce15228a63b2ef71382f7f87f5e955e82c725500
Rj1024 <ganjiraja1024@gmail.com>
2024-06-01 18:23:41 +0530

    Initial commit on key update:
    Changes to be committed:
    	modified:   src/services/api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "0a0b69e369254e498ff9a164caa4c2f0",
   },
 });
